b6d13b7e2162064e071fedbdd2a6de1de03d26e7
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-06-29 02:14:31 +0530

    updated timestamps

--- scripts/get-timestamps.php
+++ scripts/get-timestamps.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 for ($chapter = 1; $chapter <= 114; $chapter++) {
 	$url = "http://localhost:8500/v1/timestamps?chapter=$chapter";
 	$data = file_get_contents($url);
-	file_put_contents("../data/timestamps/$chapter.json", $data);
+	file_put_contents("../data/v4/timestamps/$chapter.json", $data);
 }
 
 echo "Done.";
